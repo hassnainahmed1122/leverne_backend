6461088741d0287a431c6f92aa37d03ffe7254fe
Hnahmed1122 <hnahmed@cinnova.com>
2024-07-28 13:05:23 +0500

    few updates

--- app.js
+++ app.js
@@ -2,24 +2,25 @@ const express = require('express');
 const { Sequelize } = require('sequelize');
 const cors = require('cors');
 const models = require('./models');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const allowedOrigins = [
-    '*',
-  ];
+  '*',
+];
 
 const corsOptions = {
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 
@@ -28,9 +29,30 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-  res.json({'text': "welcome"})
+  res.json({ 'text': "welcome" })
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE_DEVELOPMENT,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DIALECT,
+    logging: false,
+  }
+);
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Database connected!');
+    return sequelize.sync();
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
